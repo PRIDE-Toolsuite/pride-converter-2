1bc7734be92489b52181023f367290c77f9e9faa
Jose A Dianes <jadianes@gmail.com>
2012-03-16 15:28:53 +0000

    Identifications parser filter

--- dao-crux-txt/src/test/java/uk/ac/ebi/pride/tools/converter/dao_crux_txt/CruxTxtDaoTest.java
+++ dao-crux-txt/src/test/java/uk/ac/ebi/pride/tools/converter/dao_crux_txt/CruxTxtDaoTest.java
@@ -1,11 +1,14 @@
 package uk.ac.ebi.pride.tools.converter.dao_crux_txt;
 
 import junit.framework.TestCase;
+import uk.ac.ebi.pride.jaxb.model.Spectrum;
 import uk.ac.ebi.pride.tools.converter.dao_crux_txt.properties.ScoreCriteria;
 import uk.ac.ebi.pride.tools.converter.dao_crux_txt.properties.SupportedProperty;
 import uk.ac.ebi.pride.tools.converter.report.model.Identification;
+import uk.ac.ebi.pride.tools.converter.report.model.PTM;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Properties;
 
@@ -44,35 +47,35 @@ public class CruxTxtDaoTest extends TestCase {
             identificationsCount += newIdentification.getPeptide().size();
 //            System.out.println(newIdentification.getAccession());
         }
-        System.out.println("Identifications*peptide count = " + identificationsCount);
+//        System.out.println("Identifications*peptide count = " + identificationsCount);
     }
 
-//    public void testIdentificationByUID() throws Exception {
-//        Identification identification = cruxTxtDao.getIdentificationByUID("t_tr|DECOY_A8MVI8|A8MVI8_HUMAN(58)");
-//        assertNotNull(identification);
-//
-//        identification = cruxTxtDao.getIdentificationByUID("d_sp|Q04671-2|P_HUMAN(247)");
-//        assertNotNull(identification);
-//    }
-//
-//    public void testPTMs() throws Exception {
-//        Collection<PTM> ptms = cruxTxtDao.getPTMs();
-//
-//        assertNotNull(ptms);
-//    }
-//
-//    public void testSpectraIterator() throws Exception {
-//        int spectraCountAll = cruxTxtDao.getSpectrumCount(false);
-//        int spectraCountOnlyIdentified = cruxTxtDao.getSpectrumCount(true);
-//        Iterator<Spectrum> specAllIt = cruxTxtDao.getSpectrumIterator(false);
-//        int count = 0;
-//        while (specAllIt.hasNext()) {
-//            Spectrum newSpectrum = specAllIt.next();
-//            count++;
-////            System.out.println("Obtained spectrum: " + newSpectrum.getId());
-//        }
-//
-//        assertEquals(count,spectraCountAll);
-//
-//    }
+    public void testIdentificationByUID() throws Exception {
+        Identification identification = cruxTxtDao.getIdentificationByUID("t_tr|DECOY_A8MVI8|A8MVI8_HUMAN(58)");
+        assertNotNull(identification);
+
+        identification = cruxTxtDao.getIdentificationByUID("d_sp|Q04671-2|P_HUMAN(247)");
+        assertNotNull(identification);
+    }
+
+    public void testPTMs() throws Exception {
+        Collection<PTM> ptms = cruxTxtDao.getPTMs();
+
+        assertNotNull(ptms);
+    }
+
+    public void testSpectraIterator() throws Exception {
+        int spectraCountAll = cruxTxtDao.getSpectrumCount(false);
+        int spectraCountOnlyIdentified = cruxTxtDao.getSpectrumCount(true);
+        Iterator<Spectrum> specAllIt = cruxTxtDao.getSpectrumIterator(false);
+        int count = 0;
+        while (specAllIt.hasNext()) {
+            Spectrum newSpectrum = specAllIt.next();
+            count++;
+//            System.out.println("Obtained spectrum: " + newSpectrum.getId());
+        }
+
+        assertEquals(count,spectraCountAll);
+
+    }
 }
