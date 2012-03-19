1b7ec54ccd7dcca2005f14ae44855eb6960668b7
jadianes.ebi@gmail.com <jadianes.ebi@gmail.com@852ef0bb-aa67-d973-46e5-1eab502ac7d6>
2012-03-19 11:40:40 +0000

    Identifications parser filter

---
+++ trunk/dao-crux-txt/src/main/java/uk/ac/ebi/pride/tools/converter/dao_crux_txt/filters/DeltaCnFilterCriteria.java
@@ -0,0 +1,34 @@
+package uk.ac.ebi.pride.tools.converter.dao_crux_txt.filters;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Jose A. Dianes
+ * @version $Id$
+ */
+public class DeltaCnFilterCriteria extends FilterCriteria {
+
+    @Override
+    public boolean passFilter(Map<String, Integer> header, String[] values) {
+        double threshold = (Double)(this.getThreshold());
+        double delta = Double.parseDouble(values[header.get("delta_cn")]);
+        return (delta <= threshold);  // todo: decide if descending or ascending
+    }
+
+    @Override
+    public Object getHighestScore(Map<String, Integer> header, List<String> values) {
+
+        if ((values == null) || (values.size() == 0)) return null;
+
+        Double highest = 0.0;
+
+        for (String value :  values) {
+            String[] columns = value.split("\t");
+            highest = Math.min(highest, Double.parseDouble(columns[header.get("delta_cn")]));
+        }
+
+        return highest;
+
+    }
+}
