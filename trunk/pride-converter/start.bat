6cb0729f6f835af3a22f73ec59122d15afa446bd
rglcote@gmail.com <rglcote@gmail.com@852ef0bb-aa67-d973-46e5-1eab502ac7d6>
2012-07-06 16:29:46 +0000

    added start scripts fixed error where OtherTools HTML content wasn't properly loaded from packaged jar updated spectrast filefilter

---
+++ trunk/pride-converter/start.bat
@@ -0,0 +1,2 @@
+echo "Please ensure that the Java executable is in your PATH"
+start java -jar ./pride-converter-2.0-SNAPSHOT.jar
---
+++ trunk/pride-converter/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Please ensure that the Java executable is in your PATH"
+java -jar ./pride-converter-2.0-SNAPSHOT.jar
