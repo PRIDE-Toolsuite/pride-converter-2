3b1bfa505c1233491354619591729145e548fb91
rglcote@gmail.com <rglcote@gmail.com@852ef0bb-aa67-d973-46e5-1eab502ac7d6>
2012-11-30 16:51:47 +0000

    pride xml writer should never write spectra with empty inten/mz arrays. Instead, it will write arrays with a single double value (0.0). The EmptySpectrumFilter has been updated to handle this.

--- trunk/pride-converter/src/main/java/uk/ac/ebi/pride/tools/filter/model/impl/EmptySpectrumFilter.java
+++ trunk/pride-converter/src/main/java/uk/ac/ebi/pride/tools/filter/model/impl/EmptySpectrumFilter.java
@@ -3,6 +3,8 @@ package uk.ac.ebi.pride.tools.filter.model.impl;
 import uk.ac.ebi.pride.jaxb.model.Spectrum;
 import uk.ac.ebi.pride.tools.filter.model.Filter;
 
+import java.util.Arrays;
+
 /**
  * Created by IntelliJ IDEA.
  * User: rcote
@@ -10,6 +12,9 @@ import uk.ac.ebi.pride.tools.filter.model.Filter;
  * Time: 16:29
  */
 public class EmptySpectrumFilter implements Filter<Spectrum> {
+
+    private static final byte[] ZERO_VALUE_ARRAY = new byte[]{0, 0, 0, 0, 0, 0, 0, 0};
+
 
     @Override
     /**
@@ -23,20 +28,39 @@ public class EmptySpectrumFilter implements Filter<Spectrum> {
      */
     public boolean filter(Spectrum objectToFilter) {
 
+        //to avoid breaking the submission pipeline, the pride xml writer will
+        //create intensity/mz arrays that only contain one double, 0.0, should
+        //the DAOs return an empty spectrum. This marshalls out as follows;
+        //
+        //        <intenArrayBinary>
+        //        <data precision="64" endian="little" length="8">AAAAAAAAAAA=</data>
+        //        </intenArrayBinary>
+        //
+        //and corresponds to the byte array as defined as the ZERO_VALUE_ARRAY above
+        //
+        //we can filter on this
+
         boolean intenArrayOk = true;
         boolean mzArrayOk = true;
         if (objectToFilter.getIntenArrayBinary() == null ||
                 objectToFilter.getIntenArrayBinary().getData() == null ||
-                objectToFilter.getIntenArrayBinary().getData().getLength() == 0) {
+                objectToFilter.getIntenArrayBinary().getData().getLength() == 0 ||
+                (objectToFilter.getIntenArrayBinary().getData().getLength() == 8 &&
+                        Arrays.equals(ZERO_VALUE_ARRAY, objectToFilter.getIntenArrayBinary().getData().getValue()))
+                ) {
             intenArrayOk = false;
             System.out.println("Empty intensity array for spectrum " + objectToFilter.getId());
         }
         if (objectToFilter.getMzArrayBinary() == null ||
                 objectToFilter.getMzArrayBinary().getData() == null ||
-                objectToFilter.getMzArrayBinary().getData().getLength() == 0) {
+                objectToFilter.getMzArrayBinary().getData().getLength() == 0 ||
+                (objectToFilter.getMzArrayBinary().getData().getLength() == 8 &&
+                        Arrays.equals(ZERO_VALUE_ARRAY, objectToFilter.getMzArrayBinary().getData().getValue()))
+                ) {
             mzArrayOk = false;
             System.out.println("Empty mz array for spectrum " + objectToFilter.getId());
         }
+
         return !mzArrayOk || !intenArrayOk;
     }
 }
